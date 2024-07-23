65e434875316ce0be13ec8fdd6225e4d1c3ff7b5
Ryan Woo <whuk@users.noreply.github.com>
2024-07-23 17:19:24 +0900

    Title: 숫자 빠르게 찾기, Time: 1846ms, Memory: 51MB, Status: Passed - CodeTree

--- 240723/숫자 빠르게 찾기/find-number-fast.java
+++ 240723/숫자 빠르게 찾기/find-number-fast.java
@@ -8,18 +8,45 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int p = sc.nextInt();
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i=0;i<n;i++) {
-            map.put(sc.nextInt(), i+1);
-        }
+        // Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        // for(int i=0;i<n;i++) {
+        //     map.put(sc.nextInt(), i+1);
+        // }
 
-        for(int i=0;i<p;i++) {
-            Integer result = map.get(sc.nextInt());
-            if(result == null) {
-                System.out.println(-1);
-            } else {
-                System.out.println(result);
+        // for(int i=0;i<p;i++) {
+        //     Integer result = map.get(sc.nextInt());
+        //     if(result == null) {
+        //         System.out.println(-1);
+        //     } else {
+        //         System.out.println(result);
+        //     }
+        // }
+        int[] arr = new int[n];
+        for(int i=0;i<n; i++) {
+            arr[i] = sc.nextInt();
+        } 
+
+        for(int i=0;i<p; i++) {
+            int target = sc.nextInt();
+
+            int result = -1;
+            int left = 0;
+            int right = n-1;
+            while(left <= right) {
+                int mid = (left + right) / 2;
+                if(arr[mid] == target) {
+                    result = mid+1;
+                    break;
+                }
+
+                if(arr[mid] > target) {
+                    right = mid -1;
+                } else {
+                    left = mid + 1;
+                }
             }
+
+            System.out.println(result);
         }
     }
 }
