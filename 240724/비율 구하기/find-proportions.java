b484a18ad6b08874faaf3fbd111869247f3f1278
Ryan Woo <whuk@users.noreply.github.com>
2024-07-24 13:00:39 +0900

    Title: 비율 구하기, Time: 653ms, Memory: 22MB, Status: Wrong Answer - CodeTree

--- 240724/비율 구하기/find-proportions.java
+++ 240724/비율 구하기/find-proportions.java
@@ -21,7 +21,7 @@ public class Main {
         Iterator<Entry<String, Integer>> it = map.entrySet().iterator();
         while(it.hasNext()) {
             Entry<String, Integer> entry = it.next();
-            double rate = Math.round( Double.valueOf(entry.getValue()) / Double.valueOf(n) * 100.0) ;
+            double rate = Math.round(entry.getValue()/ (double) n * 10000 / 100) ;
             String result = String.format("%.4f", rate);
             System.out.println(entry.getKey() + " " + result);
         }
