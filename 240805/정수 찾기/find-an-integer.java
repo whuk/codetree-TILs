1d9d592de7249f758fbb62af4de978bcafe1ece7
Ryan Woo <whuk@users.noreply.github.com>
2024-08-05 13:58:05 +0900

    Title: 정수 찾기, Time: 2024ms, Memory: 70MB, Status: Passed - CodeTree

---
+++ 240805/정수 찾기/find-an-integer.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i=0; i<n; i++) {
+            set.add(sc.nextInt());
+        }
+
+        int p = sc.nextInt();
+        for(int i=0; i<p; i++) {
+            if(set.contains(sc.nextInt())) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        }
+    }
+}
