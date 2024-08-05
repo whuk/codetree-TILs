8eb80f94a46df126fb699eb7e98463021adb37cc
Ryan Woo <whuk@users.noreply.github.com>
2024-08-05 13:55:07 +0900

    Title: 서로 다른 숫자, Time: 1028ms, Memory: 39MB, Status: Passed - CodeTree

---
+++ 240805/서로 다른 숫자/distinct-numbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Set<Integer> set = new HashSet<>();
+
+        for(int i=0; i<n; i++) {
+            set.add(sc.nextInt());
+        }
+
+        System.out.println(set.size());
+    }
+}
